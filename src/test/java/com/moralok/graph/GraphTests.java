7225ab265aa4ccf0f925681738baaa373aa6bad9
moralok <moralok@163.com>
2020-09-17 00:14:47 +0800

    深度优先搜索寻找路径

--- src/test/java/com/moralok/graph/GraphTests.java
+++ src/test/java/com/moralok/graph/GraphTests.java
@@ -1,5 +1,6 @@
 package com.moralok.graph;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,16 +9,39 @@ import org.junit.Test;
  */
 public class GraphTests {
 
-    @Test
-    public void testCreateGraph() {
-        int V = 4;
+    private Graph graph;
+
+    @Before
+    public void setUp() {
+        int V = 5;
         int E = 3;
         int[] data = new int[] {0, 1, 2, 3, 1, 3};
-        Graph graph = new Graph(V, E, data);
+        graph = new Graph(V, E, data);
+    }
+
+    @Test
+    public void testGraph() {
         System.out.println("顶点的数量 " + graph.V());
         System.out.println("边的数量 " + graph.E());
-        for (int i = 0; i < V; i++) {
+        for (int i = 0; i < graph.V(); i++) {
             System.out.println("顶点 " + i + " 的邻接表 " + graph.adj(i));
+        }
+    }
+
+    @Test
+    public void testDepthFirstSearch() {
+        DepthFirstSearch search = new DepthFirstSearch(graph, 0);
+        System.out.println("与0连通的顶点数量 " + search.count());
+        for (int i = 0; i < graph.V(); i++) {
+            System.out.println("顶点 " + i + " 是否连通 "+ search.marked(i));
+        }
+    }
+
+    @Test
+    public void testDepthFirstPaths() {
+        DepthFirstPaths paths = new DepthFirstPaths(graph, 0);
+        for (int i = 0; i < graph.V(); i++) {
+            System.out.println("顶点到达 " + i + " 的路径 "+ paths.pathTo(i));
         }
     }
 }
